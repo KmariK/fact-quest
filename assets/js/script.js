79a7597973c2971139a1486ac8140414753841c3
kmariama <kmariama3@hotmail.com>
2025-04-05 00:38:47 +0100

    add event listener and functions to js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("startQuizBtn").addEventListener("click", function() {
+window.location.href = "quiz-page.html";
+});
+
+function runQuiz()
+
+function submitQuiz()
+
+function showResult ()
